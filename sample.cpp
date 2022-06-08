54c10bb620058ad54d2c8714d09674e62eeef27f
ShotaroWsd <0kz46227329345e@gmail.com>
2022-06-08 20:32:35 +0900

    branch traning branch: master sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,7 @@ int sum(int, int);
 
 int main(){
 
-    std::cout << "Git Traning" << std::endl;
+    std::cout << "This is master" << std::endl;
     int a = 5;
     int b = 3;
     std::cout << sum(a, b) << std::endl;
