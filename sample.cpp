f2984d7b6563aebc85403e4475ddd5e9e6e37441
ShotaroWsd <0kz46227329345e@gmail.com>
2022-06-08 20:37:40 +0900

    merge

--- sample.cpp
+++ sample.cpp
@@ -3,7 +3,9 @@
 int sum(int, int);
 
 int main(){
-
+    
+    std::cout << "Git Traning" << std::endl;
+    std::cout << "Git Traning" << std::endl;
     int a = 5;
     int b = 3;
     std::cout << sum(a, b) << std::endl;
